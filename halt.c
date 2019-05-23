b1f8c55cc4c274c9979024a0b4958901e01ff9f7
BerndOK <BerndOK@gmail.com>
2019-05-22 21:13:32 -0500

    Add files via upload
    Initial submit

---
+++ halt.c
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+     
+    int main()
+    {
+       int file = "input.c";
+     
+       if (f == NULL)
+          printf("HALTS\n");
+       else
+          printf("LOOPS\n");
+     
+       return 0;
+    }
